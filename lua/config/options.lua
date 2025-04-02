929af61d43833038944f187e71fdbea9f756e4ca
marky <markism1224amarille@gmail.com>
2025-04-03 00:18:52 +0800

    Initial

---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+-- Indentation settings
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.shiftwidth = 4 -- Number of spaces for each indentation level
+vim.opt.tabstop = 4 -- Number of spaces a <Tab> counts for
+vim.opt.softtabstop = 4 -- Number of spaces when pressing <Tab>
+vim.opt.autoindent = true -- Copy indentation from current line
+vim.opt.smartindent = true -- Smarter auto-indenting when starting a new line
