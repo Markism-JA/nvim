ee96b8fede20f22608d0fa3c400b5e752ebcd5ef
marky <markism1224amarille@gmail.com>
2025-12-13 21:10:19 +0800

    feat: set leader and local leader key

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,8 @@
 -- Add any additional options here
 --
 -- Indentation settings
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.opt.expandtab = true -- Convert tabs to spaces
 vim.opt.shiftwidth = 4 -- Number of spaces for each indentation level
 vim.opt.tabstop = 4 -- Number of spaces a <Tab> counts for
