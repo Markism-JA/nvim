406085849da432c3df794c07b8a00a30397d2c9e
marky <markism1224amarille@gmail.com>
2025-12-15 23:07:36 +0800

    fix: used nvim lsp instead

--- lua/plugins/powershell.lua
+++ lua/plugins/powershell.lua
@@ -1,18 +1,18 @@
-local os = require("utils.os")
-
 return {
-    "TheLeoP/powershell.nvim",
-    opts = function()
-        local bundle_path
-
-        if os.is_windows then
-            bundle_path = "AppData/Local/nvim-data/mason/packages/powershell-editor-services/"
-        elseif os.is_mac or os.is_linux then
-            bundle_path = ".local/share/nvim/mason/packages/powershell-editor-services/"
-        end
-
-        return {
-            bundle_path = bundle_path,
-        }
-    end,
+    "neovim/nvim-lspconfig",
+    opts = {
+        servers = {
+            powershell_es = {
+                bundle_path = (function()
+                    local mason_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services"
+                    return mason_path
+                end)(),
+                settings = {
+                    powershell = {
+                        codeFormatting = { Preset = "OTBS" },
+                    },
+                },
+            },
+        },
+    },
 }
