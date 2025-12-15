525a8d103740f493e1fb350f5120f80c60e93b30
marky <markism1224amarille@gmail.com>
2025-12-15 23:06:48 +0800

    chore: rzls built-in to roslyn

--- lua/plugins/Roslyn.lua
+++ lua/plugins/Roslyn.lua
@@ -3,17 +3,8 @@ return {
         "seblyng/roslyn.nvim",
         ft = { "cs", "razor" },
         dependencies = {
-            {
-                "tris203/rzls.nvim",
-                config = true,
-            },
             "mason-lspconfig.nvim",
         },
         opts = {},
-        config = function()
-            require("roslyn").setup({
-                handlers = require("rzls.roslyn_handlers"),
-            })
-        end,
     },
 }
