f4facb902f6b349b1aee07ff9f3612d4ed43dbd9
marky <markism1224amarille@gmail.com>
2025-12-15 23:07:58 +0800

    chore: added lsp and debugger pkg

--- lua/plugins/Mason.lua
+++ lua/plugins/Mason.lua
@@ -1,13 +1,25 @@
 return {
     "mason-org/mason.nvim",
+    log_level = vim.log.levels.DEBUG,
     opts = {
         ensure_installed = {
+            -- lsp
             "lua-language-server",
             "html-lsp",
             "css-lsp",
             "rust-analyzer",
             "roslyn",
-            "rzls",
+            "powershell-editor-services",
+            "jdtls",
+            "bash-language-server",
+            "clangd",
+            "ltex-ls",
+            "marksman",
+            "ruff",
+            "vtsls",
+
+            -- debugger
+            "netcoredbg",
         },
     },
 }
