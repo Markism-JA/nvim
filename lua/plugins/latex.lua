0e130ee6ad4a1acfaf4fb301c55cd632311b83eb
marky <markism1224amarille@gmail.com>
2026-02-07 01:38:32 +0800

    fix(latex): possibly fixes the sudden shutdown of the compiler

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -3,6 +3,7 @@ return {
         "lervag/vimtex",
         ft = { "tex", "bib" },
         config = function()
+            vim.g.tex_flavor = "latex"
             vim.g.vimtex_view_method = "zathura"
             vim.g.vimtex_compiler_method = "latexmk"
 
