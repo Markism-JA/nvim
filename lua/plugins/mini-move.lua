3d2ad8e493c81f37f5469863dd303c9df964e404
marky <markism1224amarille@gmail.com>
2026-02-09 19:46:42 +0800

    fix(mini-move): conflict with smart splits in binding

---
+++ lua/plugins/mini-move.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "nvim-minis/mini.move",
+        opts = {
+            mappings = {
+                -- Normal Mode
+                left = "<M-H>",
+                right = "<M-L>",
+                up = "<M-K>",
+                down = "<M-J>",
+
+                -- Visual Mode
+                line_left = "<M-H>",
+                line_right = "<M-L>",
+                line_up = "<M-K>",
+                line_down = "<M-J>",
+            },
+        },
+    },
+}
