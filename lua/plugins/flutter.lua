a82535c196eed199199c93d545e9b5f4022ae4f8
marky <markism1224amarille@gmail.com>
2026-02-17 03:12:44 +0800

    fix: make flutter config work with flutter tools

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -2,10 +2,32 @@ return {
     {
         "akinsho/flutter-tools.nvim",
         lazy = false,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "stevearc/dressing.nvim",
+            "mfussenegger/nvim-dap",
+        },
         config = function()
             local dap = require("dap")
 
             require("flutter-tools").setup({
+                ui = { border = "rounded" },
+                decorations = {
+                    statusline = { app_version = true, device = true },
+                },
+                widget_guides = { enabled = true },
+                closing_tags = {
+                    highlight = "WarningMsg",
+                    prefix = " // ",
+                    enabled = true,
+                },
+                lsp = {
+                    color = {
+                        enabled = true,
+                        virtual_text = true,
+                        virtual_text_str = "■",
+                    },
+                },
                 debugger = {
                     enabled = true,
                     run_via_dap = true,
@@ -14,11 +36,7 @@ return {
 
             dap.adapters.dart_cli = function(callback, config)
                 config.console = "externalTerminal"
-                callback({
-                    type = "executable",
-                    command = "dart",
-                    args = { "debug_adapter" },
-                })
+                callback({ type = "executable", command = "dart", args = { "debug_adapter" } })
             end
 
             dap.adapters.flutter_picker = function(callback, config)
@@ -27,11 +45,7 @@ return {
                     if choice then
                         config.toolArgs = { "-d", choice }
                         config.name = "Flutter (" .. choice .. ")"
-                        callback({
-                            type = "executable",
-                            command = "flutter",
-                            args = { "debug_adapter" },
-                        })
+                        callback({ type = "executable", command = "flutter", args = { "debug_adapter" } })
                     end
                 end)
             end
@@ -44,23 +58,30 @@ return {
                     program = "${workspaceFolder}/lib/main.dart",
                     cwd = "${workspaceFolder}",
                 },
-                {
-                    type = "dart_cli",
-                    request = "launch",
-                    name = "Dart: Run Current File",
-                    program = "${file}",
-                    cwd = "${workspaceFolder}",
-                },
-                {
-                    type = "dart_cli",
-                    request = "launch",
-                    name = "Dart: Run bin/main.dart",
-                    program = "${workspaceFolder}/bin/main.dart",
-                    cwd = "${workspaceFolder}",
-                },
             }
 
             pcall(require("dap.ext.vscode").load_launchjs, nil, { dart = { "flutter_picker", "dart_cli" } })
+        end,
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+        opts = function(_, opts)
+            opts.textobjects = opts.textobjects or {}
+
+            opts.textobjects.swap = {
+                enable = true,
+                swap_next = { ["<leader>a"] = "@parameter.inner" },
+                swap_previous = { ["<leader>A"] = "@parameter.inner" },
+            }
+        end,
+    },
+    {
+        "stevearc/conform.nvim",
+        opts = function(_, opts)
+            opts.formatters_by_ft = opts.formatters_by_ft or {}
+            opts.formatters_by_ft.dart = { "dart_format" }
         end,
     },
 }
